c7ed7cdce3d4c693d90cd8b6217b9f88dda686af
John Park <mia@dhcp-hol-2431.redrover.cornell.edu>
2020-03-09 14:56:45 -0400

    add: new UIViews

---
+++ iOS/galarm/galarm/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  galarm
+//
+//  Created by John Park on 3/9/20.
+//  Copyright © 2020 John Park. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
